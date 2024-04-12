48a048217680ae6f9fb28698a2c5226997fd63db
EnergoPro <electronigaming@gmail.com>
2024-04-12 15:42:32 +0300

    Added Color
    Controller + View

---
+++ CarDealership/CarDealership/Controllers/ColorController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarDealership.Data; 
+using CarDealership.Models;
+using Microsoft.AspNetCore.Authorization;
+
+[Authorize(Roles = "Admin")]
+public class ColorController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ColorController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    // GET: Color/Index
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var colors = _context.CarColors.ToList();
+        return View(colors);
+    }
+
+    // GET: Color/Create
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Color/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(CarColor carColor)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.CarColors.Add(carColor);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(carColor);
+    }
+
+    // GET: Color/Edit/5
+    [HttpGet]
+    public IActionResult Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var carColor = _context.CarColors.Find(id);
+
+        if (carColor == null)
+        {
+            return NotFound();
+        }
+
+        return View(carColor);
+    }
+
+    // POST: Color/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, CarColor carColor)
+    {
+        if (id != carColor.CarColorId)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Entry(carColor).State = EntityState.Modified;
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ColorExists(carColor.CarColorId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(carColor);
+    }
+
+    private bool ColorExists(int id)
+    {
+        return _context.CarColors.Any(e => e.CarColorId == id);
+    }
+}
