83a1dba3a2431f8bc8cd48ab1e3a5b734486ddb5
EnergoPro <electronigaming@gmail.com>
2024-04-12 15:40:13 +0300

    Added Brand and Model
    Controllers + Views

---
+++ CarDealership/CarDealership/Controllers/BrandController.cs
@@ -0,0 +1,99 @@
+﻿using CarDealership.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Authorize(Roles = "Admin")]
+public class BrandController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public BrandController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var brands = _context.Brands.ToList();
+        return View(brands);
+    }
+
+    // GET: Brand/Create
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Brand/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Brand brand)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Brands.Add(brand);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(brand);
+    }
+
+    // GET: Brand/Edit/5
+    [HttpGet]
+    public IActionResult Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var brand = _context.Brands.Find(id);
+        if (brand == null)
+        {
+            return NotFound();
+        }
+
+        return View(brand);
+    }
+
+    // POST: Brand/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, Brand brand)
+    {
+        if (id != brand.BrandId)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Entry(brand).State = EntityState.Modified;
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BrandExists(brand.BrandId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(brand);
+    }
+
+    private bool BrandExists(int id)
+    {
+        return _context.Brands.Any(e => e.BrandId == id);
+    }
+}
---
+++ CarDealership/CarDealership/Controllers/ModelController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarDealership.Data;
+using Microsoft.AspNetCore.Authorization;
+
+[Authorize(Roles = "Admin")]
+public class ModelController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ModelController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Model/Index
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var modelsByBrand = _context.Models
+            .Include(m => m.Brand)
+            .OrderBy(m => m.Brand.Name)
+            .ToList()
+            .GroupBy(m => m.Brand.Name); // Group models by brand name
+
+        return View(modelsByBrand);
+    }
+
+    // GET: Model/Create
+    [HttpGet]
+    public IActionResult Create()
+    {
+        ViewBag.Brands = _context.Brands.ToList();
+        return View();
+    }
+
+    // POST: Model/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Model model)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Models.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        ViewBag.Brands = _context.Brands.ToList();
+        return View(model);
+    }
+
+    // GET: Model/Edit/5
+    [HttpGet]
+    public IActionResult Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var model = _context.Models
+            .Include(m => m.Brand)
+            .FirstOrDefault(m => m.ModelId == id);
+
+        if (model == null)
+        {
+            return NotFound();
+        }
+
+        ViewBag.Brands = _context.Brands.ToList();
+        return View(model);
+    }
+
+    // POST: Model/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, Model model)
+    {
+        if (id != model.ModelId)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Entry(model).State = EntityState.Modified;
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ModelExists(model.ModelId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        ViewBag.Brands = _context.Brands.ToList();
+        return View(model);
+    }
+
+    private bool ModelExists(int id)
+    {
+        return _context.Models.Any(e => e.ModelId == id);
+    }
+}
